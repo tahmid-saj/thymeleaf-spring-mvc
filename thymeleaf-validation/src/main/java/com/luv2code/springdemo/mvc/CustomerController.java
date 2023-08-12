6446c5c9afbb7b3d8ef0261b61ed941e04e18ae9
Tahmid Sajin <tahmid.saj@gmail.com>
2023-08-12 15:03:24 -0400

    Adding customer controller

--- thymeleaf-validation/src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
+++ thymeleaf-validation/src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
@@ -1,4 +1,16 @@
 package com.luv2code.springdemo.mvc;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
 public class CustomerController {
+
+    @GetMapping("/")
+    public String showForm(Model theModel) {
+        theModel.addAttribute("customer", new Customer());
+
+        return "customer-form";
+    }
 }
