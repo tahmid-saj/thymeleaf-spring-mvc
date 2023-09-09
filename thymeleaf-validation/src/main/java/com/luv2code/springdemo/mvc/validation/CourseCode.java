17fabf6c0d1fbec40acda323f9ae8a591cae9cbd
tahmid-saj <tahmid.saj@gmail.com>
2023-09-09 14:07:59 -0400

    custom validation

--- thymeleaf-validation/src/main/java/com/luv2code/springdemo/mvc/validation/CourseCode.java
+++ thymeleaf-validation/src/main/java/com/luv2code/springdemo/mvc/validation/CourseCode.java
@@ -1,6 +1,7 @@
 package com.luv2code.springdemo.mvc.validation;
 
 import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -11,6 +12,16 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CourseCode {
+    // define default course code
+    public String value() default  "LUV";
 
+    // define default error message
+    public String message() default "must start with LUV";
+
+    // define default groups
+    public Class<?>[] groups() default {};
+
+    // define default payloads
+    public Class<? extends Payload>[] payload() default {};
 
 }
